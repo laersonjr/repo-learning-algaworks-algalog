f3ea6a09e4505f939eb7f7dc922873ce2f1ba253
Laerson Castro <laersonjr@gmail.com>
2021-11-13 23:53:18 -0300

    Criado metodo buscar cliente por id

--- src/main/java/com/laerson/treino/algalog/api/controller/ClienteController.java
+++ src/main/java/com/laerson/treino/algalog/api/controller/ClienteController.java
@@ -1,12 +1,15 @@
 package com.laerson.treino.algalog.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,26 +27,29 @@ public class ClienteController {
 	@PersistenceContext
 	private EntityManager maneger;
 	
-	/*
-	 * Consulta realizada através do JPQL, retornando uma lista de clientes
-	 * */
-	
+	// Consulta realizada através do JPQL, retornando uma lista de clientes
 	@GetMapping(value = "/teste")
 	public List<Cliente> listarClientes() {
 		return maneger.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 	
-	/*
-	 * Criando uma instancia de clienteRepository
-	 * */	
+	// Criando uma instancia de clienteRepository
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	// Metodo que retonar toda a lista de clientes
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
 	
-
+	// Meotodo que retonar cliente a partir de um ID, se não encontrar retorna status 404 not found
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		return cliente.isPresent() ? ResponseEntity.ok(cliente.get()) : ResponseEntity.notFound().build();
+	}
 	
 
 }
